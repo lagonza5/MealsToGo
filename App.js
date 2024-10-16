b4911933b8007eaac84da314f8545054f29efb28
Luis A. Gonzalez <official.lag.90@gmail.com>
2024-10-15 21:19:57 -0700

    Apply a safe area view for iOS devices

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/*
+First principles
+
+SafeAreaView to take care of iOS and iPadOS users
+View (nexted in SafeAreaView) with styling that will explicity mention the StatusBar element to avoid the status bar for Android device users.
+*/
+
+import { SafeAreaView, StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>React Native application, blank canvas</Text>
-    </View>
+    <SafeAreaView style={styles.safeContainer}>
+      <View>
+        <Text>React Native application, blank canvas</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-
+  safeContainer: {
+    flex: 1,
+  },
 });
